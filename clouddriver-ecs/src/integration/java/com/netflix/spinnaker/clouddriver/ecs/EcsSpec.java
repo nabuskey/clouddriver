93f7c448d39677ca5ba6151e66e4e7a0378a8c44
Manabu Mccloskey <manabu.mccloskey@gmail.com>
2020-11-06 16:52:50 -0700

    Separate @TestConfiguration to own class

--- clouddriver-ecs/src/integration/java/com/netflix/spinnaker/clouddriver/ecs/EcsSpec.java
+++ clouddriver-ecs/src/integration/java/com/netflix/spinnaker/clouddriver/ecs/EcsSpec.java
@@ -17,22 +17,13 @@ package com.netflix.spinnaker.clouddriver.ecs;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import com.netflix.spinnaker.cats.agent.DefaultCacheResult;
 import com.netflix.spinnaker.cats.cache.CacheData;
 import com.netflix.spinnaker.cats.cache.DefaultCacheData;
 import com.netflix.spinnaker.clouddriver.Main;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
-import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
-import com.netflix.spinnaker.clouddriver.ecs.security.NetflixAssumeRoleEcsCredentials;
-import com.netflix.spinnaker.clouddriver.ecs.security.NetflixECSCredentials;
-import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
-import com.netflix.spinnaker.credentials.CompositeCredentialsRepository;
-import com.netflix.spinnaker.credentials.definition.CredentialsParser;
+import com.netflix.spinnaker.clouddriver.ecs.test.EcsTestConfiguration;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -46,13 +37,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
+@Import(EcsTestConfiguration.class)
 @SpringBootTest(
     classes = {Main.class},
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -61,39 +51,13 @@ public class EcsSpec {
   protected static final String TEST_OPERATIONS_LOCATION =
       "src/integration/resources/testoperations";
   protected static final String TEST_ARTIFACTS_LOCATION = "src/integration/resources/testartifacts";
-  protected static final String ECS_ACCOUNT_NAME = "ecs-account";
-  protected static final String AWS_ACCOUNT_NAME = "aws-account";
+
+  @Value("${ecs.primaryAccount}")
+  protected String ECS_ACCOUNT_NAME;
+
   protected final String TEST_REGION = "us-west-2";
   protected final int TASK_RETRY_SECONDS = 3;
   protected static final String CREATE_SG_TEST_PATH = "/ecs/ops/createServerGroup";
-
-  @TestConfiguration
-  public static class Config {
-    @Bean
-    @Primary
-    public CompositeCredentialsRepository<AccountCredentials> compositeCredentialsRepository() {
-      NetflixAmazonCredentials awsCreds = TestCredential.named(AWS_ACCOUNT_NAME);
-      NetflixECSCredentials ecsCreds =
-          new NetflixAssumeRoleEcsCredentials(
-              TestCredential.assumeRoleNamed(ECS_ACCOUNT_NAME), AWS_ACCOUNT_NAME);
-      CompositeCredentialsRepository<AccountCredentials> repo =
-          mock(CompositeCredentialsRepository.class);
-      when(repo.getCredentials(any(), eq("aws"))).thenReturn(awsCreds);
-      when(repo.getCredentials(any(), eq("ecs"))).thenReturn(ecsCreds);
-      when(repo.getFirstCredentialsWithName(ECS_ACCOUNT_NAME)).thenReturn(ecsCreds);
-      when(repo.getFirstCredentialsWithName(AWS_ACCOUNT_NAME)).thenReturn(awsCreds);
-      return repo;
-    }
-
-    @Bean("amazonCredentialsParser")
-    @Primary
-    public CredentialsParser amazonCredentialsParser() {
-      NetflixAmazonCredentials awsCreds = TestCredential.assumeRoleNamed(ECS_ACCOUNT_NAME);
-      CredentialsParser parser = mock(CredentialsParser.class);
-      when(parser.parse(any())).thenReturn(awsCreds);
-      return parser;
-    }
-  }
 
   @Value("${ecs.enabled}")
   Boolean ecsEnabled;
